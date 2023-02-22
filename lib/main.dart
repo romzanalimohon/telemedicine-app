4fb77eaf380f93d0f8c5452ab3ea0f3ca48c7a2d
Rais Uddin <raisdhaka@gmail.com>
2023-02-22 18:42:45 +0600

    rotues

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:telemedecine_app/ui_model/home.dart';
 import 'package:telemedecine_app/ui_model/profiledemo.dart';
 import 'package:telemedecine_app/ui_model/status.dart';
 
+import '../utils/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,8 +21,16 @@ class MyApp extends StatelessWidget {
       //home: LoginPage(),
       //home: ProfileDemoPage(),
       //home: HomePage(),
-      home: RegisterPage(),
+      // home: RegisterPage(),
       //home: Status(),
+
+      initialRoute: MyRoutes.LoginRoute,
+      routes: {
+        // MyRoutes.ProfileRoute: (context) => ProfileDemoPage(),
+        MyRoutes.ConsultantRoute: (context) => HomePage(),
+        MyRoutes.LoginRoute: (context) => LoginPage(),
+        MyRoutes.RegisterRoute: (context) => RegisterPage(),
+      },
     );
   }
 }
---
+++ lib/utils/routes.dart
@@ -0,0 +1,8 @@
+class MyRoutes {
+  static String LoginRoute = '/login';
+  static String ConsultantRoute = '/';
+  static String ProfileRoute = '/profile';
+  static String SettingRoute = '/setting';
+  static String StatusRoute = '/status';
+  static String RegisterRoute = '/register';
+}
