a6e9dc11bb9394cc97860ae6cbeefb218ba42058
romzanalimohon <romzanalimohon@gmail.com>
2023-03-27 15:10:55 +0600

    token update

--- lib/for_video_calling/utils/settings.dart
+++ lib/for_video_calling/utils/settings.dart
@@ -1,6 +1,5 @@
 
 
-
 const channelId = "test";
 const appId = "4d7467fcc77841fea7d25cde32798890";
-const token = "007eJxTYGiYejX9tnTY/ifCPC/sH69K0r+wZmV+gNvtrxIXzkgr6rQoMJikmJuYmaclJ5ubW5gYpqUmmqcYmSanpBobmVtaWFgaeAh/T24IZGTYxXmCgREKQXwWhpLU4hIGBgCEyR/a";
+const token = "007eJxTYEhS8kiPP7TY+/7T6t1cm7PXNtcFrHj+dtr14D+Oh+7Lt9YrMJikmJuYmaclJ5ubW5gYpqUmmqcYmSanpBobmVtaWFga2EcqpjQEMjJcf+vAxMgAgSA+C0NJanEJAwMA1VshHg==";
